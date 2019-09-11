75992822081eb1128918c00d6105d83494e99f3c
andrepimenta <andrepimenta7@gmail.com>
2019-09-11 18:07:01 +0100

    Implement SharedStorage

---
+++ ios/SharedStorage.h
@@ -0,0 +1,13 @@
+//
+//  SharedStorage.h
+//  hodl
+//
+//  Created by Andre Pimenta on 19/09/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "React/RCTBridgeModule.h"
+
+@interface SharedStorage : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/SharedStorage.m
@@ -0,0 +1,39 @@
+//
+//  SharedStorage.m
+//  hodl
+//
+//  Created by Andre Pimenta on 19/09/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "SharedStorage.h"
+#import "React/RCTLog.h"
+
+@implementation SharedStorage
+
+RCT_EXPORT_MODULE();
+
+// We can send back a promise to our JavaScript environment :)
+RCT_EXPORT_METHOD(set:(NSString *)data
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  @try{
+    NSUserDefaults *shared = [[NSUserDefaults alloc]initWithSuiteName:@"group.com.createwidget.pimenta"];
+    [shared setObject:data forKey:@"data"];
+    [shared synchronize];
+    NSLog(@"%@",data);
+    NSArray *keys = [[shared dictionaryRepresentation] allKeys];
+    
+    for(NSString* key in keys){
+      // your code here
+      NSLog(@"value: %@ forKey: %@",[shared valueForKey:key],key);
+    }
+    resolve(@"true");
+  }@catch(NSException *exception){
+    reject(@"get_error",exception.reason, nil);
+  }
+  
+}
+
+@end
